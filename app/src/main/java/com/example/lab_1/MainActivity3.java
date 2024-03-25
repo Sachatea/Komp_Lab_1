0eface66cdd486e9127aa741b08fb31aa4a3bf6a
Sachatea <sachaomelyna@gmail.com>
2024-03-25 11:41:04 +0200

    fin

---
+++ app/src/main/java/com/example/lab_1/MainActivity3.java
@@ -0,0 +1,41 @@
+package com.example.lab_1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Intent intent = getIntent();
+        if(intent != null) {
+            String word = intent.getStringExtra("word_key");
+
+            TextView textView = findViewById(R.id.textView);
+            textView.setText("Параметр: " + word);
+
+
+        }
+
+
+
+
+
+    }
+}
